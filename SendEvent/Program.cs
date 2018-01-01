a22c66a73b87ac410b10988f84966ed7fae9625f
kheiakiyama <kheiakiyama@gmail.com>
2018-01-01 18:13:50 +0900

    プロジェクト ファイルを追加します。

---
+++ SendEvent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SendEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
